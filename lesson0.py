ff89e32dd9e92f04ca91fb15e01a6c95f0217066
fievski <fievski@yandex.ru>
2024-06-06 15:33:27 +0600

    lesson0.py

--- lesson0.py
+++ lesson0.py
@@ -1,6 +1,12 @@
 example = 'Топинамбур'
 print(example[0:1])
-print(example[9:])
+print(example[-1:])
 print(example[5:10])
 print(example[::-1])
-print(example[0:-1:2])
+print(example[1::2])
+
+#Т
+#р
+#амбур
+#рубманипоТ
+#оиабр
